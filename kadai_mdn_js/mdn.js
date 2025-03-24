b6f17c12a72b277422f26e1191164aed8e4e1b7d
akir34890 <aki_kondo46@hotmail.com>
2025-03-24 18:52:14 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+
+
+const today = new Date();
+//const year = today.getFullYear();
+//const month = today.getMonth();
+//const date = today.getDate();
+
+console.log(today.getFullYear() + "年" + (today.getMonth() + 1) + "月" + today.getDate() + "日");
+
+
+
+
+
